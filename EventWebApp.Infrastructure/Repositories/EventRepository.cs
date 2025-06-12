e03d2f63e87e3c53bd994bbbfd02dd375b9b1972
Publo <111525871+Candymanzz@users.noreply.github.com>
2025-06-12 12:23:54 +0300

    feat: add repositories

---
+++ EventWebApp.Infrastructure/Repositories/EventRepository.cs
@@ -0,0 +1,109 @@
+﻿using EventWebApp.Application.DTOs;
+using EventWebApp.Application.Interfaces;
+using EventWebApp.Core.Model;
+using EventWebApp.Infrastructure.Date;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace EventWebApp.Infrastructure.Repositories
+{
+    public class EventRepository : IEventRepository
+    {
+        private readonly AppDbContext appDbContext;
+
+        public EventRepository (AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public async Task AddAsync(Event _event)
+        {
+            appDbContext.Events.Add(_event);
+            await appDbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var _event = await appDbContext.Events.FindAsync(id);
+            if (_event != null)
+            {
+                appDbContext.Events.Remove(_event);
+                await appDbContext.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Event>> GetAllAsync()
+        {
+            return await appDbContext.Events.Include(e => e.Users).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Event>> GetByFiltersAsync(string? category, string? location, DateTime? dateTime)
+        {
+            var query = appDbContext.Events.AsQueryable();
+
+            if (!string.IsNullOrEmpty(category))
+            {
+                query = query.Where(e => e.Category == category);
+            }
+
+            if (!string.IsNullOrEmpty(location))
+            {
+                query = query.Where(e => e.Location == location);
+            }
+
+            if (dateTime.HasValue)
+            {
+                query = query.Where(e => e.DateTime.Date == dateTime.Value.Date);
+            }
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<Event?> GetByIdAsync(Guid id)
+        {
+            return await appDbContext.Events.Include(e => e.Users).FirstOrDefaultAsync(e => e.Id == id);
+        }
+
+        public async Task<Event?> GetByTitleAsync(string title)
+        {
+            return await appDbContext.Events.FirstOrDefaultAsync(e => e.Title == title);
+        }
+
+        public async Task UpdateAsync(Event _event)
+        {
+            appDbContext.Events.Update(_event);
+            await appDbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateImageAsync(Guid id, string imageUrl)
+        {
+            var ev = await appDbContext.Events.FindAsync(id);
+            if (ev == null)
+                throw new InvalidOperationException("Event not found");
+
+            ev.ImageUrl = imageUrl;
+            await appDbContext.SaveChangesAsync();
+        }
+
+        public async Task<PaginatedResult<Event>> GetPagedAsync(int pageNumber, int pageSize)
+        {
+            var query = appDbContext.Events.Include(e => e.Users).AsQueryable();
+
+            var total = await query.CountAsync();
+
+            var items = await query
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return new PaginatedResult<Event>
+            {
+                Items = items,
+                TotalCount = total,
+                PageNumber = pageNumber,
+                PageSize = pageSize
+            };
+        }
+
+    }
+}
---
+++ EventWebApp.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,94 @@
+﻿using EventWebApp.Core.Model;
+using EventWebApp.Infrastructure.Date;
+using EventWebApp.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventWebApp.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext appDbContext;
+
+        public UserRepository (AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public async Task AddAsync(User user)
+        {
+            appDbContext.Users.Add(user); // AddAsync????
+            await appDbContext.SaveChangesAsync();
+        }
+
+        public async Task CancelUserFromEvent(Guid userId, Guid eventId)
+        {
+            var user = await appDbContext.Users.Include(u => u.Events).FirstOrDefaultAsync(u => u.Id == userId);
+            var _event = await appDbContext.Events.Include(e => e.Users).FirstOrDefaultAsync(e => e.Id == eventId);
+
+            if (_event == null || user == null)
+            {
+                throw new InvalidOperationException("User or Event not found.");
+            }
+
+            if (_event.Users.Contains(user))
+            {
+                _event.Users.Remove(user);
+            }
+        }
+
+        public async Task<User?> GetByIdAsync(Guid id)
+        {
+            return await appDbContext.Users.Include(u => u.Events).FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task<IEnumerable<User>> GetUsersByEvent(Guid id)
+        {
+            var _event = await appDbContext.Events.Include(e => e.Users).FirstOrDefaultAsync(e => e.Id == id);
+
+            return _event?.Users ?? Enumerable.Empty<User>(); //null
+        }
+
+        public async Task RegisterUserToEventAsync(Guid userId, Guid eventId)
+        {
+            var user = await appDbContext.Users.Include(u => u.Events).FirstOrDefaultAsync(u => u.Id == userId);
+            var _event = await appDbContext.Events.Include(e => e.Users).FirstOrDefaultAsync(e => e.Id == eventId);
+
+            if (_event == null || user == null)
+            {
+                throw new InvalidOperationException("User or Event not found.");
+            }
+
+            if (!_event.Users.Contains(user))
+            {
+                _event.Users.Add(user);
+            }
+
+            await appDbContext.SaveChangesAsync();
+        }
+
+        public async Task<User?> GetByEmailAsync(string email)
+        {
+            return await appDbContext.Users
+                .Include(u => u.Events)
+                .FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<User?> GetByRefreshTokenAsync(string refreshToken)
+        {
+            return await appDbContext.Users
+                .FirstOrDefaultAsync(u => u.RefreshToken == refreshToken);
+        }
+
+        public async Task UpdateRefreshTokenAsync(Guid userId, string refreshToken, DateTime expiry)
+        {
+            var user = await appDbContext.Users.FindAsync(userId);
+            if (user is null) return;
+
+            user.RefreshToken = refreshToken;
+            user.RefreshTokenExpiryTime = expiry;
+
+            await appDbContext.SaveChangesAsync();
+        }
+
+    }
+}
