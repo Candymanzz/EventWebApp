0d40f08f7c1343dae2d5839d42c6cc7ff57538d7
Publo <111525871+Candymanzz@users.noreply.github.com>
2025-06-12 12:20:17 +0300

    feat: add mapping

---
+++ EventWebApp.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using EventWebApp.Core.Model;
+using EventWebApp.Application.DTOs;
+
+namespace EventWebApp.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<Event, EventDto>().ForMember(dest => dest.CurrentParticipantsCount,
+                opt => opt.MapFrom(src => src.Users.Count));
+
+            CreateMap<CreateEventRequest, Event>();
+
+            CreateMap<UpdateEventRequest, Event>();
+
+            CreateMap<UserRegistrationRequest, User>().ForMember(dest => dest.RegistrationDate,
+                opt => opt.MapFrom(_ => DateTime.UtcNow));
+
+            CreateMap<User, UserDto>();
+        }
+    }
+}
